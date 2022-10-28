c3c3de8f56b9a4de2ef06628ebdb4ea19a88b645
thsc42 <thomas.schwotzer@htw-berlin.de>
2022-10-28 11:02:18 +0200

    byte failure implemented - there is a bug, though. Random is not as random as expected

--- src/networks/utils/InputFailureSimulatorWrapper.java
+++ src/networks/utils/InputFailureSimulatorWrapper.java
@@ -1,10 +1,15 @@
 package networks.utils;
 
+import net.sharksystem.utils.AlarmClock;
+import net.sharksystem.utils.AlarmClockListener;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 
-class InputFailureSimulatorWrapper extends InputStream {
+class InputFailureSimulatorWrapper extends InputStream implements AlarmClockListener {
+    public static final int MAX_READ_WAIT_MILLIS = Integer.MAX_VALUE;
+//    public static final int MAX_READ_WAIT_MILLIS = 500;
     private final InputStream sourceIS;
     private final int byteFailureRate;
     private final Random random;
@@ -15,10 +20,63 @@ class InputFailureSimulatorWrapper extends InputStream {
         this.random = new Random(System.currentTimeMillis());
     }
 
+    private class ByteReaderThread extends Thread {
+        private final InputStream is;
+        int readInt;
+        boolean successfullyRead = false;
+        IOException ioException = null;
+
+        ByteReaderThread(InputStream is) throws IOException {
+            this.is = is;
+        }
+
+        public void run() {
+            try {
+                this.readInt = is.read();
+            } catch (IOException e) {
+                this.ioException = e;
+            }
+        }
+    }
+
+    private Thread threadWaitingForReadingToEnd = null;
+
+    @Override
+    public void alarmClockRinging(int i) {
+        if(this.threadWaitingForReadingToEnd != null) {
+            this.threadWaitingForReadingToEnd.interrupt();
+            this.threadWaitingForReadingToEnd = null;
+        }
+    }
+
     @Override
     public int read() throws IOException {
         while (true) { // you are trapped here - escape: read byte or IOException
-            int readByte = this.sourceIS.read();
+            // start read thread
+            ByteReaderThread readThread = new ByteReaderThread(this.sourceIS);
+
+            // set an alarm before reading
+            this.threadWaitingForReadingToEnd = Thread.currentThread();
+            AlarmClock alarmClock = new AlarmClock(MAX_READ_WAIT_MILLIS, this);
+            alarmClock.start();
+
+            // start..
+            readThread.start();
+            // .. and wait
+            try {
+                readThread.join();
+                alarmClock.kill();
+            } catch (InterruptedException e) {
+                // alarm kicked in
+                throw new IOException("stopped reading - max idle time exceeded");
+            }
+
+            // problems while reading
+            if(readThread.ioException != null) throw readThread.ioException;
+
+            // no problems - get read byte
+            int readByte = readThread.readInt;
+            if(readByte == -1) return -1; // eof
 
             // no failure
             if (byteFailureRate <= 0) return readByte;
@@ -28,11 +86,15 @@ class InputFailureSimulatorWrapper extends InputStream {
                 // simulate failure?
                 int randomNumber = this.random.nextInt();
                 randomNumber = randomNumber < 0 ? -randomNumber : randomNumber;
-                if (randomNumber % (100-this.byteFailureRate) != 0) {
+                randomNumber %= 100;
+                // 99 nearly always... 1 hardly
+
+                if (randomNumber > this.byteFailureRate) {
                     // no
                     return readByte;
                 }
             }
         }
     }
+
 }
--- tests/networks/utils/FailureSimulatorTests.java
+++ tests/networks/utils/FailureSimulatorTests.java
@@ -6,9 +6,27 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 
 public class FailureSimulatorTests {
     public static final int NUMBER_DATA = 10000;
+
+    @Test
+    public void randomTest() {
+        Random random = new Random(System.currentTimeMillis());
+
+        int sum = 0;
+        for(int i = 0; i < NUMBER_DATA; i++) {
+            int randomNumber = random.nextInt();
+            randomNumber = randomNumber < 0 ? -randomNumber : randomNumber;
+            randomNumber %= 100;
+
+            sum += randomNumber;
+        }
+
+        int average = sum / NUMBER_DATA;
+        System.out.println("average == " + average);
+    }
 
     @Test
     public void test1() {
@@ -26,6 +44,12 @@ public class FailureSimulatorTests {
     public void test50() {
         System.out.println("failure rate 50% ");
         this.testRun(50);
+    }
+
+    @Test
+    public void test99() {
+        System.out.println("failure rate 99% ");
+        this.testRun(99);
     }
 
     public void testRun(int byteFailureRate) {
